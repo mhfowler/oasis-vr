ae54c674a7257f50257167014a80f916d9e71eed
Max Fowler <maxhfowler@gmail.com>
2016-02-03 15:18:29 -0500

    website

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,8 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("html/*.html").on('change', browserSync.reload);
-    gulp.watch("css/*.css").on('change', browserSync.reload);
+    //gulp.watch("html/*.html", ['index_html']);
+    gulp.watch(["html/*.html","css/*.css"], ['index_html']).on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -21,9 +21,15 @@ gulp.task('sass', function() {
     return gulp.src("scss/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("css"))
+        ;
+});
+
+// rebuild index.html
+gulp.task('index_html', function() {
+    return gulp.src("html/*.html")
         .pipe(shell([
             'python build_index.py',
-            'echo "cat3" > test2.txt'
+            'echo "cat4" > test2.txt'
         ], {
             templateData: {
                 f: function (s) {
@@ -32,7 +38,6 @@ gulp.task('sass', function() {
         }
         }))
         ;
-        //.pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
