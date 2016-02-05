35bd2fdea516a02b0ad302cf26e46141514cf72b
Max Fowler <maxhfowler@gmail.com>
2016-02-05 16:36:11 -0500

    auto

--- js/portfolio_on_main.js
+++ js/portfolio_on_main.js
@@ -1,4 +1,13 @@
 
 $(document).ready(function() {
   alert('hello');
+
+  $(document).scroll(function() {
+    if ($(document).scrollTop() > 200) {
+        $('.navbar').fadeIn();
+    }
+    else {
+      $('.navabr').hide();
+    }
+})
 });
