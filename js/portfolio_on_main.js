91cde08855ca2c9d983c726c0e5e4351cc0fce38
Max Fowler <maxhfowler@gmail.com>
2016-02-08 15:40:21 -0500

    auto

--- js/portfolio_on_main.js
+++ js/portfolio_on_main.js
@@ -8,5 +8,11 @@ $(document).ready(function() {
     else {
       //$('.navabr').hide();
     }
+
+    $('.project-row').click(function(e) {
+        e.preventDefault();
+        var link = $(this).data('link');
+        window.open(link, '_blank');
+    });
 })
 });
