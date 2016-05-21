4881dcd55dbdaa0c80595d51410e26f0467fa6c7
Max Fowler <maxhfowler@gmail.com>
2016-05-21 02:13:02 -0400

    auto

--- js/oasis.js
+++ js/oasis.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     binary_div.html(bin);
     setInterval(function(){
             var bin = binary_div.html();
-            for (i = 0; i < 100; i++) {
+            for (i = 0; i < 1; i++) {
                 if (Math.random() < 0.5) {
                     bin = '1' + bin;
                 }
