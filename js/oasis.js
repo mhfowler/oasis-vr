b89b76fd003e7341844d0ece462ebaab74094b6f
Max Fowler <maxhfowler@gmail.com>
2016-05-21 02:23:38 -0400

    auto

--- js/oasis.js
+++ js/oasis.js
@@ -3,31 +3,31 @@ $(document).ready(function() {
 
     var palm_tree = $('.palm-emoji');
 
-    var binary_div = $(".binary");
-
-    var bin = '10';
-    for (i = 0; i < 5000; i++) {
-        if (Math.random() < 0.5) {
-            bin += '1';
-        }
-        else {
-            bin += '0'
-        }
-    }
-    binary_div.html(bin);
-
-    setInterval(function(){
-            var bin = '10';
-            for (i = 0; i < 5000; i++) {
-                if (Math.random() < 0.5) {
-                    bin = '1' + bin;
-                }
-                else {
-                    bin = '0' + bin;
-                }
-            }
-            binary_div.html(bin); },
-        10000);
+    //var binary_div = $(".binary");
+    //
+    //var bin = '10';
+    //for (i = 0; i < 5000; i++) {
+    //    if (Math.random() < 0.5) {
+    //        bin += '1';
+    //    }
+    //    else {
+    //        bin += '0'
+    //    }
+    //}
+    //binary_div.html(bin);
+    //
+    //setInterval(function(){
+    //        var bin = '10';
+    //        for (i = 0; i < 5000; i++) {
+    //            if (Math.random() < 0.5) {
+    //                bin = '1' + bin;
+    //            }
+    //            else {
+    //                bin = '0' + bin;
+    //            }
+    //        }
+    //        binary_div.html(bin); },
+    //    10000);
 
 
     //setInterval(function(){
